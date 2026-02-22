7dc08a17a24d7bd13fd4ec5f1f0ba5e485a21c4a
FRANCIA MOSQUERA <francia.mosquera@jikkosoft.com>
2026-02-22 15:09:29 -0500

    Implementar Extension para separar responsabilidades de la configuración del builder y del app en métodos de extensión.

---
+++ WebApiSmartCard/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace WebApiSmartCard.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static WebApplication UseSmartCardPipeline(this WebApplication app)
+        {
+            // Configuración del HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/openapi/v1.json", "Tu Smart Card v1");
+                });
+            }
+
+            app.UseCors(options =>
+            {
+                options.AllowAnyHeader();
+                options.AllowAnyMethod();
+                options.AllowAnyOrigin();
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
